de6f1b7c60f185b5c8931646d1788e2a5b3e15dc
jean <carlosjean808@gmail.com>
2020-03-02 21:48:26 -0400

    primeiro commit

---
+++ aula.py
@@ -0,0 +1,19 @@
+A=int(input('digite um número '))
+B=int(input('digite um segundo número '))
+
+c =(A+B)/2
+print("média entre os 2 números é: {}".format(c))
+
+C=int(input('digite um número '))
+D=int(input('digite um segundo número '))
+
+F =(C+D)/2
+print("média entre os 2 números é: {}".format(F))
+
+E=int(input('digite um número '))
+G=int(input('digite um segundo número '))
+
+H =(E+G)/2
+print("média entre os 2 números é: {}".format(H))
+
+print("média :{0}, {1} e {2}".format(C,F,H))
---
+++ aula2.py
@@ -0,0 +1,21 @@
+def calcular_media2(X,Y):
+    P=(X+Y)/2
+    print(P)
+    return P
+    
+
+def calcular_media():
+    A=int(input("digite primeiro número"))
+    B=int(input("digite segundo número"))
+    D=(A+B)/2
+    print(D)
+    return D
+
+#E=calcular_media()
+#F=calcular_media()
+#G=calcular_media()
+#print("{0},{1},{2}".format(E,F,G))
+    
+A=int(input("digite primeiro número"))
+B=int(input("digite segundo número"))
+C=calcular_media2(A,B)
